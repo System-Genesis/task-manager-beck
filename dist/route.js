54ba79150c7c83b8e4a195c0f295e459a164d093
YoniZangolsaz <77918500+YoniZangolsaz@users.noreply.github.com>
2022-03-09 21:07:10 +0200

    Delete dist directory

--- dist/route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-router.get('/login', (_req, res) => {
-    res.send('sabba');
-});
-exports.default = router;
-//# sourceMappingURL=route.js.map
